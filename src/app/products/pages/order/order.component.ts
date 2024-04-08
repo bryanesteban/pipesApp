9006035e99fb33cb2839f341812f524bef029c2a
bryanesteban <bryanesteban_123@hotmail.com>
2024-04-08 14:25:49 -0500

    pipe for orderBy

--- src/app/products/pages/order/order.component.ts
+++ src/app/products/pages/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Color, Hero } from '../../Interfaces/hero.interface';
 
 @Component({
   selector: 'products-order',
@@ -7,9 +8,39 @@ import { Component } from '@angular/core';
 })
 export class OrderComponent {
 
-  public isUpperCase: boolean = true;
+  public isUpperCase: boolean = false;
+  public orderBy?: keyof Hero ;
+
+
+  public heroes: Hero[] =[
+   { name: 'Superman',
+    canFly: true,
+    color: Color.blue
+  },
+   { name: 'Batman',
+    canFly: false,
+    color: Color.black
+  },
+   { name: 'Daredevil',
+    canFly: false,
+    color: Color.red
+  },
+   { name: 'Robin',
+    canFly: false,
+    color: Color.red
+  },
+   { name: 'Linterna Verde',
+    canFly: true,
+    color: Color.green
+  },
+
+  ]
 
   toggleUpperCase():void {
     this.isUpperCase = !this.isUpperCase
+  }
+
+  changeOrder( value: keyof Hero){
+    this.orderBy = value;
   }
 }
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -8,6 +8,8 @@ import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.compo
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { OrderComponent } from './pages/order/order.component';
 import { ToggleCasePipe } from './pipes/toggle-case.pipe';
+import { CanFlyPipe } from './pipes/canFly.pipe';
+import { SortByPipe } from './pipes/sort-by.pipe';
 
 
 @NgModule({
@@ -18,7 +20,9 @@ import { ToggleCasePipe } from './pipes/toggle-case.pipe';
     OrderComponent,
 
     //Pipes
-    ToggleCasePipe
+    ToggleCasePipe,
+    CanFlyPipe,
+    SortByPipe
   ],
   imports: [
     CommonModule,
